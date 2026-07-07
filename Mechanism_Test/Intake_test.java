afbe0600b7f49df5ea74a603f7fe89f38f3faf07
unforev <146119019+unforev@users.noreply.github.com>
2026-07-07 13:10:27 +0700

    Add files via upload

---
+++ Mechanism_Test/Intake_test.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Mechanism_Test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.Mechanism.Intake;
+
+@TeleOp
+public class Intake_test extends OpMode {
+
+    private Intake Adudu_Intake;
+    private Gamepad Adudu_Gamepad1;
+
+    @Override
+    public void init() {
+        Adudu_Intake.Initiate();
+    }
+
+    @Override
+    public void loop() {
+        if(Adudu_Gamepad1.x)
+        {
+            Adudu_Intake.Operate();
+            Adudu_Intake.stop();
+            Adudu_Gamepad1.x = false;
+        }
+    }
+}
---
+++ Mechanism_Test/Outtake_test.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.Mechanism_Test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.Mechanism.Outtake;
+
+
+@TeleOp
+public class Outtake_test extends OpMode {
+
+    private Outtake Adudu_Outtake;
+    private Gamepad Adudu_Gamepad1;
+
+    @Override
+    public void init(){
+        Adudu_Outtake.Initiate();
+    }
+
+    @Override
+    public void loop(){
+
+        if( Adudu_Gamepad1.b)
+        {
+            Adudu_Outtake.Operate(1);
+            Adudu_Outtake.update();
+            Adudu_Gamepad1.b = false;
+        }
+
+        if(Adudu_Gamepad1.y)
+        {
+            Adudu_Outtake.Operate(2);
+            Adudu_Outtake.Reset_Wait();
+            Adudu_Gamepad1.y = false;
+        }
+
+
+    }
+
+}
